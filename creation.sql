d155e8a63173f183f805d3a71adbe63812a7cef5
Patrick Chiang <pchiang@uw.edu>
2015-01-12 21:53:19 -0800

    changed user table name and properties, added user_info table

--- creation.sql
+++ creation.sql
@@ -4,15 +4,25 @@ CREATE DATABASE isopro;
 
 USE isopro;
 
-CREATE TABLE users
+CREATE TABLE user
 (
 user_id int NOT NULL AUTO_INCREMENT,
 user_email varchar(128) NOT NULL,
-user_username varchar(128),
+user_username varchar(128) NOT NULL,
 user_password_salt varchar(128) NOT NULL,
 user_password_hash varchar(128) NOT NULL,
 PRIMARY KEY (user_id),
 CONSTRAINT unique_username UNIQUE (email),
 CONSTRAINT unique_email UNIQUE (email)
+);
+
+CREATE TABLE user_info
+(
+user_id int NOT NULL,
+info_first_name varchar(128),
+info_last_name varchar(128),
+info_city varchar(128),
+info_phone_number varchar(128),
+FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
