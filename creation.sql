bd416ff51042e2d05433b4587f9218c01e1b5d06
Patrick Chiang <pchiang@uw.edu>
2015-01-12 21:55:57 -0800

    create message table, user table now working

--- creation.sql
+++ creation.sql
@@ -12,8 +12,8 @@ user_username varchar(128) NOT NULL,
 user_password_salt varchar(128) NOT NULL,
 user_password_hash varchar(128) NOT NULL,
 PRIMARY KEY (user_id),
-CONSTRAINT unique_username UNIQUE (email),
-CONSTRAINT unique_email UNIQUE (email)
+CONSTRAINT unique_username UNIQUE (user_username),
+CONSTRAINT unique_email UNIQUE (user_email)
 );
 
 CREATE TABLE user_info
@@ -26,3 +26,13 @@ info_phone_number varchar(128),
 FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
+CREATE TABLE message
+(
+message_id int NOT NULL,
+user_id_sender int NOT NULL,
+user_id_receiver int NOT NULL,
+message_text varchar(128),
+message_read_status varchar(128),
+FOREIGN KEY (user_id_sender) REFERENCES user(user_id),
+FOREIGN KEY (user_id_receiver) REFERENCES user(user_id)
+);
